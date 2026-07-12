e8dbbc0b75e3a91fb147e14ee5526aa2afeb1a43
jinjiantong <jinjiantong@yeah.net>
2026-07-12 23:27:44 +0800

    fix: suppress hydration warning for data-theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
   }
 
   return (
-    <html lang="zh-CN">
+    <html lang="zh-CN" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
